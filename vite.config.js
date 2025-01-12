177fb2d3548154b682f32bc2a7ba4dbad6e6327c
saketrajabhishek <saketrajabhishek@gmail.com>
2025-01-13 03:26:46 +0530

    add PWA support with service worker and manifest for Stake Game

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,36 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  // Replace <REPO_NAME> with your repository name
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      manifest: {
+        name: "Stake Game",
+        short_name: "StakeGame",
+        description:
+          "Play the new generation Stake Game and Tic-Tac-Toe. Add a bet and win cash prizes.",
+
+        display: "standalone",
+        background_color: "#ffffff",
+        theme_color: "#000000",
+        icons: [
+          {
+            src: "/icons/icon-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "/icons/icon-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+    }),
+  ],
+});
